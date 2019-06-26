33324600bdbc714aa09b226509e5dd42724ec770
Hannah Ripley <d00317171@dmail.dixie.edu>
2019-06-26 12:46:22 -0600

    connected server to client

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var port= process.env.PORT || 3000;
 //Data
 var data = require( "./data.js");
 
+
+
 //Middleware
 server.use(express.urlencoded( {
 	extended: false
@@ -17,42 +19,23 @@ server.use (express.json());
 
 //REST endpoints
 
-server.get("/names", function (req, res) {
-	console.log(data);
-	var response = { names: data.names };
-	
-	res.json(response);
+server.get("/posts", function (req,res) {
+    var response = {posts : data.posts};
+    res.json(response);
 });
-
-server.post("/names", function (req, res) {
-	if (!req.body.name ) {
-		res.status(400);
-		var response = { msg: "please submit a name"};
-		res.json(response);
-
-	} else if (req.body.name == "" || req.body.name == "Addison") {
-		res.status(400);
-		var response = { msg: "please enter a real name"};
-		res.json(response);
-	} else {
-		data.names.push (req.body.name);
-		res.status(201);
-		res.send();
-	}
-});
-
-
-
-server.get("/numbers", function (req,res) {
-	var response = { numbers: data.numbers };
-	res.json(response);
-});
-server.post("/numbers", function(req,res) {
-	data.numbers.push(req.body.number);
+server.post("/posts", function (req, res) {
+    var new_post = {
+        title: req.body.title,
+        author: req.body.author,
+        category: req.body.category,
+        image: req.body.image,
+        text: req.body.text,
+        date: req.body.date
+    };
+    data.posts.unshift(new_post);
 	res.status(201);
 	res.send();
 });
-
 
 
 server.listen(port, function() {
